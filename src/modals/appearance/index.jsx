ae4f016387ea483770b76165ae9aa67a7b5fb277
ayberkkk <ayberktiglii@gmail.com>
2023-10-03 17:22:26 +0300

    #8
    home tab

--- src/modals/appearance/index.jsx
+++ src/modals/appearance/index.jsx
@@ -163,6 +163,7 @@ export default function AppearanceModal({ close }) {
                   setBackgroundColor({
                     name: "light",
                     primary: "#fff",
+                    primaryAlpha: "#ffffffa6",
                     secondary: "#f7f9f9",
                     third: "#eff3f4",
                     modal: "#00000066",
@@ -211,6 +212,7 @@ export default function AppearanceModal({ close }) {
                   setBackgroundColor({
                     name: "dark",
                     primary: "#15202b",
+                    primaryAlpha: "#15202ba6",
                     secondary: "#1e2732",
                     third: "#263340",
                     modal: "#5b708366",
@@ -259,6 +261,7 @@ export default function AppearanceModal({ close }) {
                   setBackgroundColor({
                     name: "darker",
                     primary: "#000",
+                    primaryAlpha: "#000000a6",
                     secondary: "#16181c",
                     third: "#212327",
                     modal: "#5b708366",
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,19 @@
+import StickyHeader from "../../components/sticky-header";
+import Tab from "../../components/tab";
+
 export default function Home() {
-    return (
-        <div>
-            Home Component !
-        </div>
-    )
+  return (
+    <>
+      <Tab activeTab="followings">
+        <StickyHeader title="Anasayfa">
+          <Tab.Items>
+            <Tab.Item id="for-you">Sana özel</Tab.Item>
+            <Tab.Item id="followings">Takip edilenler</Tab.Item>
+          </Tab.Items>
+        </StickyHeader>
+        <Tab.Content id="for-you">1. content</Tab.Content>
+        <Tab.Content id="followings">2. content</Tab.Content>
+      </Tab>
+    </>
+  );
 }
